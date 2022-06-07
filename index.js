140034e91443c87aea056df2bd3855368f1d27df
Edsey omenda <omendajames0317@gmail.com>
2022-06-07 17:34:42 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+
+  if(distance >=2501  ){
+    return 'No can do.'
+  }else if(distance >=2001){
+    return 'I will gladly take your thirty bucks.'
+  }else if(distance <=400){
+    return 'This one is on me!'
+  }
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(destination){
   // Write your code here!
+  return destination === 'NYC'? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return  "Thank you.";
+      break;
+    default:
+      return  "Bye.";
+  }
 }
